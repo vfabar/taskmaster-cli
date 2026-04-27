7bb5916b4cc30938877db3b9a909df46aefe9476
vfabar <vi.fabar@duocuc.cl>
2026-04-27 12:08:55 -0400

    feat: add task command

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const {readTasks} = require('./tasks');
+const {readTasks, writeTasks} = require('./tasks');
 
 const [,, cmd] = process.argv;
 
-if (cmd=== 'list') {
-    const tasks = readTasks();
-    if (tasks.length === 0) {
-        console.log('No hay tareas.');
-    } else {
-        tasks.forEach((t, i) => {
-            const icon = t.done ? '✅' : '❌';
-            console.log('\${icon} [\${i+1}] \${t.title}');
-        });
-    }
+const title = process.argv.slice(3).join(' ');
+
+if (cmd === 'list') {
+  const tasks = readTasks();
+  if (tasks.length === 0) {
+    console.log('No hay tareas.');
+  } else {
+    tasks.forEach((t, i) => {
+      const icon = t.done ? '✔' : '○';
+      console.log(`${icon} [${i+1}] ${t.title}`);
+    });
+  }
 }
+
+if (cmd === 'add') {
+    if (!title) {
+        console.log('Usa: node index.js add "<título>"');
+        process.exit(1);
+    }
+    const tasks = readTasks();
+    tasks.push({ id: Date.now(), title, done: false});
+    writeTasks(tasks);
+    console.log(' Tarea '+ title +' añadida.');
+}
+
